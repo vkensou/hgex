9f3172da53b54945944691c53338468926a6e7da
vkensou <boywanyx@163.com>
2024-02-26 23:09:36 +0800

    shader采样贴图，正常显示贴图

--- src/core/hge.ps.hlsl
+++ src/core/hge.ps.hlsl
@@ -5,8 +5,13 @@ struct VSOutput
     unorm float4 Color : TEXCOORD1;
 };
 
+[[vk::binding(0, 0)]]
+Texture2D texture : register(t0);
+[[vk::binding(1, 0)]]
+SamplerState texture_sampler : register(s0);
+
 [shader("pixel")]
 float4 main(VSOutput input) : SV_TARGET
 {
-    return input.Color;
+    return input.Color * texture.Sample(texture_sampler, input.UV);
 }
